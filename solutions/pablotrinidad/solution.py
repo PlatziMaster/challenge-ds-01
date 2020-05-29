a3fa2fda783b736d9004ad91780b6f6afa526e03
Pablo Trinidad <p@pablotrinidad.me>
2020-05-29 15:59:04 -0500

    Added dummy answer with Jupyter Notebook

--- solutions/pablotrinidad/solution.py
+++ solutions/pablotrinidad/solution.py
@@ -1,6 +1,10 @@
 """Solution by pablotrinidad."""
 
+import os
 from typing import Tuple
+
+import numpy as np
+import pandas as pd
 
 
 class Solution:
@@ -8,8 +12,16 @@ class Solution:
 
     def analyze_books(self, books_dataset: str) -> Tuple[int, int, int]:
         """Returns the n-th fibonacci number."""
-        books_count = 0
+        books = pd.read_csv(books_dataset, error_bad_lines=False)
+        print(books.dtypes)
+        book_count = 0
         with open(books_dataset, "r") as bds:
             for _ in bds.readlines():
-                books_count += 1
-        return (1, 2, books_count)
+                book_count += 1
+        print(f"Found {book_count} books! which is > {np.pi}")
+
+        return (
+            int(os.getenv("SOLUTION_QUESTION_1")),
+            int(os.getenv("SOLUTION_QUESTION_2")),
+            int(os.getenv("SOLUTION_QUESTION_3")),
+        )
